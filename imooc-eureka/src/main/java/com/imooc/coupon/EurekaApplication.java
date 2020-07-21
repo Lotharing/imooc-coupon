9a0c09f1257b5439e3f865e01b4d0eaea94dafc2
Lotharing <809676295@qq.com>
2020-07-21 20:01:07 +0800

    企业级优惠卷初始化:EurekaServer

---
+++ imooc-eureka/src/main/java/com/imooc/coupon/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.imooc.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka 启动入口
+ * @author : LuTong.Zhao
+ * @date : 19:34 2020/7/20
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
