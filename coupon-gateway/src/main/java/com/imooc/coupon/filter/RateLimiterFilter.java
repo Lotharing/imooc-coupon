99d7e60a3e7d207a8a427a92dbe265f1da7ffd31
Lotharing <809676295@qq.com>
2020-07-22 18:43:49 +0800

    RateLimiter 令牌桶原理 - 请求限流

---
+++ coupon-gateway/src/main/java/com/imooc/coupon/filter/RateLimiterFilter.java
@@ -0,0 +1,41 @@
+package com.imooc.coupon.filter;
+
+import com.google.common.util.concurrent.RateLimiter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 限流过滤器： 某个IP在时间断内的请求次数限制  , 系统的请求的时间限制等
+ * @author : LuTong.Zhao
+ * @date : 18:35 2020/7/22
+ */
+@Slf4j
+@Component
+@SuppressWarnings("all")
+public class RateLimiterFilter extends AbstractPreZuulFilter{
+    /**创建限流器,令牌桶规则：每秒可以获取2个令牌**/
+    RateLimiter rateLimiter = RateLimiter.create(2.0);
+
+    /**
+     * 自定义过滤器去完成
+     * @return
+     */
+    @Override
+    protected Object cRun() {
+        HttpServletRequest request = context.getRequest();
+        if (rateLimiter.tryAcquire()){
+            log.info("get rate token success");
+            return success();
+        }else{
+            log.error("rate limit:{}",request.getRequestURI());
+            return fail(402,"error: rate limit");
+        }
+    }
+
+    @Override
+    public int filterOrder() {
+        return 2;
+    }
+}
