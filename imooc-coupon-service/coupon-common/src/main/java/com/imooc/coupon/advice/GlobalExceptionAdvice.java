dd4b5ff29ac848b1267aa6e02fe3701ed26e18aa
Lotharing <809676295@qq.com>
2020-07-23 19:48:05 +0800

    统一异常封装/Advice对Controller拦截增强/ExceptionHandler进行处理定义的全局异常/返回统一异常信息

---
+++ imooc-coupon-service/coupon-common/src/main/java/com/imooc/coupon/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,30 @@
+package com.imooc.coupon.advice;
+
+import com.imooc.coupon.exception.CouponException;
+import com.imooc.coupon.vo.CommonResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 全局异常处理
+ * @author : LuTong.Zhao
+ * @date : 19:40 2020/7/23
+ */
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+    /**
+     * @ExceptionHandler对指定的异常进行拦截,异常处理器 对CouponException进行统一处理
+     * @param request
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(value = CouponException.class)
+    public CommonResponse<String> handlerCouponException(HttpServletRequest request, CouponException ex){
+        CommonResponse<String> response = new CommonResponse<>(-1,"business error");
+        response.setData(ex.getMessage());
+        return response;
+    }
+
+}
---
+++ imooc-coupon-service/coupon-common/src/main/java/com/imooc/coupon/exception/CouponException.java
@@ -0,0 +1,13 @@
+package com.imooc.coupon.exception;
+
+/**
+ * 优惠卷项目通用异常定义
+ * @author : LuTong.Zhao
+ * @date : 19:39 2020/7/23
+ */
+public class CouponException extends Exception{
+
+    public CouponException(String message){
+        super(message);
+    }
+}
